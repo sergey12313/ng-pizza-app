2bf6d7efee8bac7f56f27a4da072c5dce87f03f8
sergey12313 <sergey12313@gmail.com>
2023-10-03 21:57:21 +0300

    feat: define tailwind and css vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#fe724c',
+        primaryHover: '#fe724c',
+        muted: '#5b5b5e',
+        separator: "#d4d6e0"
+      },
+    },
   },
   plugins: [],
 }
